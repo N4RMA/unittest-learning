7d7ec12c5ac9dcca706fbfe8e7fad1040e1479c5
N4RMA <manar-yacine@outlook.com>
2023-04-17 23:40:53 -0400

    added edge cases

--- test_calc.py
+++ test_calc.py
@@ -3,9 +3,24 @@ import calc
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        result = calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-10, 5), -5)
+        self.assertEqual(calc.add(0, -5), -5)
 
+    def test_sub(self):
+        self.assertEqual(calc.sub(16, 4), 12)
+        self.assertEqual(calc.sub(-16, 4), -20)
+        self.assertEqual(calc.sub(1, -1), 2)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(16, 4), 64)
+        self.assertEqual(calc.multiply(-1, 4), -4)
+        self.assertEqual(calc.multiply(0, -1), 0)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(16, 4), 4)
+        self.assertEqual(calc.divide(1, 4), -4)
+        self.assertEqual(calc.divide(0, -1), 0)
 
 if __name__ == '__main__':
     unittest.main()
